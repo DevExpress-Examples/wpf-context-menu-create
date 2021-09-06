bb3b20c0ffe8cab16f2ab575ac70d2b01ef3e4df
DevExpressExampleBot <fakeEmail>
2021-09-06 23:00:14 +0300

    Source auto update [skip ci]

--- VB/DXContextMenu_Ex/Application.xaml.vb
+++ VB/DXContextMenu_Ex/Application.xaml.vb
@@ -1,11 +1,11 @@
 ﻿Imports System.Windows
 
 Namespace DXContextMenu_Ex
-    ''' <summary>
-    ''' Interaction logic for App.xaml
-    ''' </summary>
-    Partial Public Class App
-        Inherits Application
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
 
-    End Class
+	End Class
 End Namespace
