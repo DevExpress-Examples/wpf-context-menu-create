d5abb86c3bdacb6b24ebd86451302e6d3bcd6dad
DevExpressExampleBot <fakeEmail>
2023-12-07 17:52:38 +0400

    Source auto update [skip ci]

--- VB/DXContextMenu_Ex/Window1.xaml.vb
+++ VB/DXContextMenu_Ex/Window1.xaml.vb
@@ -1,46 +1,35 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace DXContextMenu_Ex
-	''' <summary>
-	''' Interaction logic for Window1.xaml
-	''' </summary>
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub itemCopy_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			textEdit1.Copy()
-		End Sub
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        End Sub
 
-		Private Sub itemPaste_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			textEdit1.Paste()
-		End Sub
+        Private Sub itemCopy_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Me.textEdit1.Copy()
+        End Sub
 
-		Private Sub itemUndo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			textEdit1.Undo()
-		End Sub
+        Private Sub itemPaste_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Me.textEdit1.Paste()
+        End Sub
 
-		Private Sub itemRedo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			textEdit1.Redo()
-		End Sub
-	End Class
+        Private Sub itemUndo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Me.textEdit1.Undo()
+        End Sub
+
+        Private Sub itemRedo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Me.textEdit1.Redo()
+        End Sub
+    End Class
 End Namespace
