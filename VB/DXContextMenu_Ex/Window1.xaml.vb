63e77a3abb430698f6a10d7affbcb0acc63bd3d6
DevExpressExampleBot <fakeEmail>
2023-12-07 17:54:56 +0400

    Source auto update [skip ci]

--- VB/DXContextMenu_Ex/Window1.xaml.vb
+++ VB/DXContextMenu_Ex/Window1.xaml.vb
@@ -1,27 +1,28 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace DXContextMenu_Ex
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub itemCopy_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            textEdit1.Copy()
+            Me.textEdit1.Copy()
         End Sub
 
         Private Sub itemPaste_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            textEdit1.Paste()
+            Me.textEdit1.Paste()
         End Sub
 
         Private Sub itemUndo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            textEdit1.Undo()
+            Me.textEdit1.Undo()
         End Sub
 
         Private Sub itemRedo_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-            textEdit1.Redo()
+            Me.textEdit1.Redo()
         End Sub
     End Class
 End Namespace
